f29a34e9716cf2c65c2fe2b802cd21befba7df9e
sanam1060 <sanam_g1990@yahoo.com>
2022-07-26 18:08:18 -0400

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //createTaskHandler() function to dynamically create the task item
 // Having this separate function makes our code more organized and clear
 
-var createTaskHandler = function (){
+var createTaskHandler = function (event){
+  event.preventDefault();
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
@@ -12,4 +13,4 @@ var createTaskHandler = function (){
 }
 
 //On a button clikc, create a task
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
